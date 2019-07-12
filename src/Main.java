214af4bd24ee84939037c82c75b7de625ce26f75
bscnarola <bscnarola@gmail.com>
2019-07-12 11:15:40 +0100

    testing git bash

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
             {
                 System.out.println(mat[i][j]);
                 System.out.println("scr tree test");
+                System.out.println("testing git bash");
 
             }
 
